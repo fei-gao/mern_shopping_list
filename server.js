3b91d69afaa0d1f363390c4771266f6ede21ce44
Fei Gao <fgao1@ualberta.ca>
2018-11-11 14:57:15 -0700

    run both client and server concurrently by editing package.json file

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const items = require('./routes/api/items');
 const app = express();
 
 // bodyparser middleware
@@ -15,6 +16,9 @@ const db = require('./config/keys').mongoURI;
 mongoose.connect(db, { useNewUrlParser: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
+
+// use routes
+app.use('/api/items', items);
 
 const port = process.env.PORT || 5001;
 app.listen(port, () => {
